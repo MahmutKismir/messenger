d87d14190ec5de9c367e5017b3399ad542223508
Mahmut Kismir <103948581+MahmutKismir@users.noreply.github.com>
2023-06-06 13:34:27 +0300

    fixed

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,81 +1,84 @@
-import getCurrentUser from "@/app/actions/getCurrentUsers";
 import { NextResponse } from "next/server";
-import prisma from "@/app/libs/prismadb";
-import { pusherServer } from "@/app/libs/pusher";
 
-export async function POST(request: Request) {
+import getCurrentUser from "@/app/actions/getCurrentUsers";
+import { pusherServer } from '@/app/libs/pusher'
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(
+  request: Request,
+) {
   try {
     const currentUser = await getCurrentUser();
     const body = await request.json();
-    const { message, image, conversationId } = body;
+    const {
+      message,
+      image,
+      conversationId
+    } = body;
 
     if (!currentUser?.id || !currentUser?.email) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse('Unauthorized', { status: 401 });
     }
 
     const newMessage = await prisma.message.create({
+      include: {
+        seen: true,
+        sender: true
+      },
       data: {
         body: message,
         image: image,
         conversation: {
-          connect: {
-            id: conversationId,
-          },
+          connect: { id: conversationId }
         },
         sender: {
-          connect: {
-            id: currentUser.id,
-          },
+          connect: { id: currentUser.id }
         },
         seen: {
           connect: {
-            id: currentUser.id,
-          },
+            id: currentUser.id
+          }
         },
-      },
-      include: {
-        seen: true,
-        sender: true,
-      },
+      }
     });
 
-    const updateConversation = await prisma.conversation.update({
+    
+    const updatedConversation = await prisma.conversation.update({
       where: {
-        id: conversationId,
+        id: conversationId
       },
       data: {
         lastMessageAt: new Date(),
         messages: {
           connect: {
-            id: newMessage.id,
-          },
-        },
+            id: newMessage.id
+          }
+        }
       },
       include: {
         users: true,
         messages: {
           include: {
-            seen: true,
-          },
-        },
-      },
+            seen: true
+          }
+        }
+      }
     });
 
-    await pusherServer.trigger(conversationId, 'messages:new', newMessage)
+    await pusherServer.trigger(conversationId, 'messages:new', newMessage);
 
-    const lastMessage = updateConversation.messages[updateConversation.messages.length - 1]
+    const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1];
 
-    updateConversation.users.map((user) => {
-      pusherServer.trigger(user.email!, 'conversation:update' , {
+    updatedConversation.users.map((user) => {
+      pusherServer.trigger(user.email!, 'conversation:update', {
         id: conversationId,
         messages: [lastMessage]
-      })
-    })
+      });
+    });
 
     return NextResponse.json(newMessage)
-
-  } catch (error: any) {
-    console.log(error, "ERROR_MESSAGES");
-    return new NextResponse("InternalError", { status: 500 });
+  } catch (error) {
+    console.log(error, 'ERROR_MESSAGES')
+    return new NextResponse('Error', { status: 500 });
   }
 }
